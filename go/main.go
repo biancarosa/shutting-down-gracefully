4ad079f8aed76b4010cd3f85df0ad0abab6dc9a6
Bianca Rosa <me@biancarosa.com.br>
2021-12-12 21:10:37 -0300

    Add long running go routine

--- go/main.go
+++ go/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	N    = 50
+	Mins = 1
+)
 
 func main() {
 	fmt.Println("Hello world")
+	for i := 0; i < N; i++ {
+		fmt.Printf("%d of %d", i, N)
+		go func() {
+			fmt.Println("A slow running goroutine....")
+			time.Sleep(Mins * time.Minute)
+		}()
+	}
 }
