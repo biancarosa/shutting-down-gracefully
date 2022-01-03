1408624e248e2255511e4cff4706f4eeace4be54
Bianca Rosa <brosa@redhat.com>
2022-01-03 15:37:11 -0300

    Update code to use wait groups

--- go/main.go
+++ go/main.go
@@ -2,35 +2,30 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
+	"sync"
 	"time"
 )
 
 const (
-	N    = 50
-	Mins = 1
+	N       = 50
+	Seconds = 10
 )
 
 func main() {
 
 	fmt.Println("Hello world")
+	var wg sync.WaitGroup
 	for i := 0; i < N; i++ {
-		fmt.Printf("%d of %d", i, N)
+		fmt.Printf("%d of %d\n", i, N)
+		wg.Add(1)
 		go func() {
-			fmt.Println("A slow running goroutine....")
-			time.Sleep(Mins * time.Minute)
+			fmt.Println("A slow running goroutine has started....")
+			time.Sleep(Seconds * time.Second)
+			wg.Done()
+			fmt.Println("A slow running goroutine is done....")
 		}()
 	}
 
-	gracefulStop := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt)
-		<-sigint
-		close(gracefulStop)
-	}()
-
-	<-gracefulStop
+	wg.Wait()
 	fmt.Println("Everything has shut down, goodbye")
 }
