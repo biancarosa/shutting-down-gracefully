6148636e0903ad3be6d12f6dcf2f78cd5b6ab494
Bianca Rosa <brosa@redhat.com>
2022-01-07 12:16:56 -0300

    Remove outside signal listening

--- go/main.go
+++ go/main.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	N       = 50
-	Seconds = 10
+	Seconds = 100
 )
 
 type Status string
@@ -40,36 +40,29 @@ func main() {
 		}
 		wg.Add(1)
 		tasks[i] = &task
+		go func() {
+			sigint := make(chan os.Signal, 1)
+			signal.Notify(sigint, os.Interrupt)
+			fmt.Println("Waiting for signal internally...")
+			<-sigint
+			fmt.Println("Received an interrupt internally..")
+			// Is it possible to have a race condition here? Yes.
+			wg.Done()
+			task.Status = Interrupted
+			fmt.Printf("Interrupted Task ID %d\n", task.ID)
+
+		}()
 		go func(t *Task) {
 			fmt.Println("A slow running goroutine started....")
 			time.Sleep(Seconds * time.Second)
 			fmt.Println("A slow running goroutine finished....")
 			task.Status = Finished
-			go func() {
-				sigint := make(chan os.Signal, 1)
-				signal.Notify(sigint, os.Interrupt)
-				<-sigint
-				// Is it possible to have a race condition here? Yes.
-				wg.Done()
-				task.Status = Interrupted
-			}()
 			wg.Done()
 		}(&task)
 	}
-
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt)
-		fmt.Println("Waiting for signal...")
-		<-sigint
-		fmt.Println("Received an interrupt...")
-		wg.Wait()
-		for i, t := range tasks {
-			fmt.Printf("Index %d of Task ID %d has status %s\n", i, t.ID, t.Status)
-		}
-	}()
-
 	wg.Wait()
-
+	for _, t := range tasks {
+		fmt.Printf("Task ID %d has status %s\n", t.ID, t.Status)
+	}
 	fmt.Println("Everything has shut down, goodbye")
 }
