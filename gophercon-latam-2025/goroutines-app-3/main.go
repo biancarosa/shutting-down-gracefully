d233ab7a2577e10fa45168f1880e773a25cbf6f9
Bianca Rosa <me@biancarosa.com.br>
2025-05-03 21:05:46 -0300

    update example

--- gophercon-latam-2025/goroutines-app-3/main.go
+++ gophercon-latam-2025/goroutines-app-3/main.go
@@ -5,8 +5,7 @@ import (
 	"time"
 
 	"os"
-	"os/signal"
-	"syscall"
+	"sync"
 )
 
 
@@ -15,27 +14,23 @@ const (
 	Seconds = 10
 )
 
+var wg sync.WaitGroup
+
 func main() {
 	fmt.Println("Hello world")
+
 	for i := 0; i < N; i++ {
 		fmt.Printf("%d of %d: Scheduling go routine\n", i, N)
+		wg.Add(1)
 		go func(i int) {
 			fmt.Printf("%d of %d: A slow running goroutine started....\n", i, N)
 			time.Sleep(Seconds * time.Second)
 			fmt.Printf("%d of %d: A slow running goroutine finished....\n", i, N)
+			wg.Done()
 		}(i)
 	}
+	
+	wg.Wait()
 
-
-	gracefulStop := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
-		<-sigint
-		fmt.Println("Shutting down gracefully...")
-		close(gracefulStop)
-	}()
-
-	<-gracefulStop
 	fmt.Println("Everything has shut down, goodbye")
 }
